309a4cb0102ab2df7b68c36c0f1e0330d35475b8
Vimaxno1 <ifeanyiprogress70@gmail.com>
2024-07-28 18:37:49 +0100

    update on api

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+
+import SearchIcon from "@mui/icons-material/Search";
+
+import { useStyles } from "./styles.js";
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@mui/material";
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  console.log(onPlaceChanged);
+  console.log({ onLoad });
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,80 @@
+import React, { createRef, useEffect, useState } from "react";
+
+import useStyles from "./styles";
+import {
+  CircularProgress,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+const List = ({
+  places,
+  type,
+  setType,
+  rating,
+  setRating,
+  childClicked,
+  isLoading,
+}) => {
+  const classes = useStyles();
+
+  const [elRefs, setElrefs] = useState([]);
+
+  useEffect(() => {
+    const refs = Array(places.length)
+      .fill()
+      .map((_, i) => elRefs[i] || createRef());
+    setElrefs(refs);
+  }, [places]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attractions around yoou
+      </Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid item key={i} xs={12}>
+                <PlaceDetails
+                  place={place}
+                  selected={Number(childClicked) === i}
+                  refProp={elRefs[i]}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default List;
